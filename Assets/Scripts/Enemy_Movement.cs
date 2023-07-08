6a3d8af8aaf4041a59822f0076981ca0ab00d469
week3 <donnyweek3@gmail.com>
2023-07-08 15:38:58 -0500

    Added optional adjusting enemy range overlay for debug. Added health bars. Added ability for enemy to attack for variable damage with variable range/delay.

--- Assets/Scripts/Enemy_Movement.cs
+++ Assets/Scripts/Enemy_Movement.cs
@@ -9,16 +9,34 @@ public class Enemy_Movement : MonoBehaviour
     GameObject target;
 
     public string target_tag = "adventurer";
+    public float range = 1.5f;
+    private float rangeSquared;
+    public bool showRange = true;
+    public float attackDelay = 2f; //Seconds between attacks
+    public float damage = 0.5f;
+    private float nextAttackTime=0f; //minimum clock time till next attack
+
+    public Transform rangeOverlay;
     public float movementSpeed = 1f; // Adjust this value to set the movement speed
     private Rigidbody2D rb2d;
+
+
     // Start is called before the first frame update
     void Start()
     {
         adventurers = GameObject.FindGameObjectsWithTag(target_tag);
         rb2d = GetComponent<Rigidbody2D>();
+        rangeSquared=range*range;
+        setRangeOverlay();
     }
 
-    GameObject findClosest()
+    private void FixedUpdate() 
+    {
+        handleMovement();
+        handleAttack();
+    }
+
+    private GameObject findClosest()
     {
         float distance = Mathf.Infinity;
         Vector3 pos = transform.position;
@@ -35,7 +53,10 @@ public class Enemy_Movement : MonoBehaviour
         return closest_obj;
     }
 
-    private void FixedUpdate() {
+    //Called every game tick
+    //Moves enemy to closest target
+    private void handleMovement()
+    {
         if(target==null)
         {
             target = findClosest();
@@ -48,6 +69,42 @@ public class Enemy_Movement : MonoBehaviour
             direction = direction*movementSpeed;
             rb2d.velocity = new Vector2(direction.x, direction.y);
         }
+    }
 
+    private void setRangeOverlay()
+    {
+        float scale;
+        if(showRange)
+            scale=range*2;
+        else
+            scale=0f;
+        
+        Vector3 localScale = rangeOverlay.localScale;
+        rangeOverlay.localScale =new Vector3(scale,scale,localScale.z); //Set healthbar scale to the percent of health left
+    }
+
+    //Get the squared distance from this object to the other
+    //SQRT function is very slow. So just compare to the squared distance
+    private float getSquareDistance2D(Vector3 v)
+    {
+        Vector3 distance = transform.position - v;
+        distance.z=0;
+        return distance.sqrMagnitude;
+    }
+
+    private void handleAttack()
+    {
+        if(target != null)
+        {
+            float currentTime=Time.time;
+            if(currentTime>nextAttackTime)
+            {
+                if(getSquareDistance2D(target.transform.position)<=rangeSquared)
+                {
+                    nextAttackTime=currentTime+attackDelay;
+                    target.SendMessage("ApplyDamage",damage);
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/KnightMovement.cs
+++ Assets/Scripts/KnightMovement.cs
@@ -5,16 +5,47 @@ using UnityEngine;
 public class KnightMovement : MonoBehaviour
 {
     public float movementSpeed = 5f; // Adjust this value to set the movement speed
+    public float maxHealth = 10f;
+    private float currentHealth;
+    public Transform healthbarTransform;
     private Rigidbody2D rb2d;
+
     private void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>(); //Map physics object
+        currentHealth=maxHealth;
     }
     private void FixedUpdate()
+    {
+        handleMovement();
+        updateHealth();
+    }
+
+    //Movement handler called every game tick
+    //Velocity direction handled by wasd for debugging
+    private void handleMovement()
     {
         float horizontalInput = Input.GetAxis("Vertical");
         float verticalInput = Input.GetAxis("Horizontal");
 
         rb2d.velocity = new Vector2(verticalInput*movementSpeed,horizontalInput*movementSpeed);
+    }
+
+    //Health handler called every game tick
+    private void updateHealth()
+    {
+        Vector3 localScale = healthbarTransform.localScale;
+        healthbarTransform.localScale =new Vector3(currentHealth/maxHealth,localScale.y,localScale.z); //Set healthbar scale to the percent of health left
+    }
+
+    //Function other objects can call to apply damage. Has to be named this exactly
+    public void ApplyDamage(float damage)
+    {
+        currentHealth-=damage;
+        if(currentHealth<0)
+        {
+            currentHealth=0;
+        }
+        updateHealth();
     }
 }
